c60fb68d94088af9941b149dd407568fbb1a2315
student-of-software <nurks1905@gmail.com>
2022-09-19 00:51:49 +0300

    ders 6

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrSayisi = 12;
+		String mesaj = "Ogrenci sayisi: ";
+		System.out.println(mesaj+ogrSayisi);
+		System.out.println("Ogrenci sayisi: " +ogrSayisi);
+	}
+
+}
